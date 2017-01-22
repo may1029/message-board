490ab6e0fd5ad223bebadd702f263397f451c10c
may1029 <mymy.a1029@gmail.com>
2017-01-22 17:10:07 +0900

    add age function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # validates :age, numericality: {only_integer: true, less_than_or_equal_to: 150}
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -43,9 +43,10 @@ class MessagesController < ApplicationController
     redirect_to root_path, notice: 'メッセージを削除しました'
   end
 
+
   private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body, :age)
   end
   ## ここまで
   def set_message
